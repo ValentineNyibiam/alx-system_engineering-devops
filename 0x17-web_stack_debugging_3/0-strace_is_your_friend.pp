b40bd7b1cc6d135f02361886fa242b63a3ca7415
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-06-06 04:07:32 +0100

    build(alx-se-devops:0x17-web_stack_debugging_3): add a manifest that edits a file

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# A puppet manifest to replace a line in a file on a server
+
+exec { 'replace_line':
+	command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+	path    => ['/bin', '/usr/bin']
+}
