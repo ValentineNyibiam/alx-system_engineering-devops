927995c81d7714a00c4047f7a44f1b96d91aa1ba
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-08-17 10:54:04 +0100

    build(alx-se-devops:0x16): adjust function syntax to meet pycodestyle standards

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """
 prints the titles of the first 10 hot
 posts listed for a given subreddit
@@ -18,19 +17,17 @@ def top_ten(subreddit):
     if subreddit is None or not isinstance(subreddit, str):
         print("None")
 
-    user_agent = {'User-agent': \
-                  'Google Chrome Version 81.0.4044.129'}
+    user_agent = \
+        {'User-agent': 'Google Chrome Version 81.0.4044.129'}
     params = {'limit': 10}
-    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
+    url = f'https://www.reddit.com/r/{subreddit}/hot/.json'
 
-    response = get(url, headers=user_agent, params=params)
+    response = get(url, headers=user_agent, params=params, timeout=10)
     results = response.json()
 
     try:
         my_data = results.get('data').get('children')
-
         for i in my_data:
             print(i.get('data').get('title'))
-
-    except Exception:
-        print("None")
+    except (AttributeError, KeyError, TypeError) as e:
+        print(f"An unexpected error occured: {e}")
